456553d0f7040ef0f2d8b6b2b025c22be647fe64
aiiaaiiaaiia <nuttrawitra.k@ku.th>
2021-02-26 22:03:01 +0700

    Status : connecting python modules

--- develop/utils.py
+++ develop/utils.py
@@ -1,5 +1,6 @@
 from import_python_library import *
 
+############### LANGUAGE ################
 def lang(language):
   if language == 'detect':
      code_lang = 'all'
@@ -39,3 +40,29 @@ def translang(translanguage):
   else: 
     code_translang = 'en'
   return code_translang
+
+##################### IMAGE ######################
+
+def normalize(arr):
+    rng = arr.max()-arr.min()
+    amin = arr.min()
+    return (arr-amin)*255/rng
+
+def compare_images(frame, prev_frame):
+    current_frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    current_frame_gray_edge = cv2.Canny(current_frame_gray,100,200)
+    prev_frame_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)    
+    prev_frame_gray_edge = cv2.Canny(prev_frame_gray,100,200)
+
+    img1 = normalize(current_frame_gray_edge)
+    img2 = normalize(prev_frame_gray_edge)
+    diff = img1 - img2  
+    m_norm = np.sum(abs(diff))  # Manhattan norm
+    # z_norm = norm(diff.ravel(), 0)  # Zero norm
+    return (m_norm)
+
+def roi_simlarity(roi, prev_roi):
+    g_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    g_prev_roi = cv2.cvtColor(prev_roi, cv2.COLOR_BGR2GRAY)
+
+    method = cv2.TM_SQDIFF_NORMED
--- develop/video.py
+++ develop/video.py
@@ -1,198 +1,239 @@
+# -*- coding: utf-8 -*-
 from import_python_library import *
-from def_init import *
-from def_frame_similarity import *
+from utils import *
 
-##------ INIT ------##
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", type=str,
-	help="path to input video")
-ap.add_argument("-p", "--position", type=str,
-	help="position to overlay the text")
-ap.add_argument("-l", "--language", type=str,
-	help="the input languages")
-ap.add_argument("-t", "--translanguage", type=str,
-	help="the translate language")
-args = vars(ap.parse_args())
+class RT_ATRT():
+	def __init__(self):
+		ap = argparse.ArgumentParser()
+		ap.add_argument("-v", "--video", type=str,
+		help="path to input video")
+		ap.add_argument("-p", "--position", type=str,
+		help="position to overlay the text")
+		ap.add_argument("-l", "--language", type=str,
+		help="the input languages")
+		ap.add_argument("-t", "--translanguage", type=str,
+		help="the translate language")
+		args = vars(ap.parse_args())
+		s = 30                          # default font size 
+		language = args["language"]     # video language
+		translanguage = args["translanguage"]
 
-videopath = args["video"]    
-overlay = args["position"]      # above, below, text position
-code_color = (0,0,255)          # default color : red
-s = 30                          # default font size 
-language = args["language"]     # video language
-translanguage = args["translanguage"]     
-font = ImageFont.truetype('angsau_0.ttf', s)
-# para = False                  # default is False
-code_lang, reader = lang(language)
-code_translang = translang(translanguage)
+		############ Parameter #############
+		self.videopath = args["video"]
+		self.overlay = args["position"]      # above, below, text position
+		self.frame_similarity_threshold = 100
+		self.roi_distance_threshold = 6.0
+		self.multi_input_language = language
+		self.font = ImageFont.truetype('angsau_0.ttf', s)
+		# para = False                  # default is False
+		self.code_lang, self.reader = lang(language)
+		self.code_translang = translang(translanguage)
+		self.code_color = (0,0,255)          # default color : red
 
-##----start process about video----##
-cap = cv2.VideoCapture(videopath) 
-if (cap.isOpened()== False): 
-    print("Error opening video stream or file")
-ret, frame = cap.read()
-previous_frame = frame
 
-fps = cap.get(cv2.CAP_PROP_FPS)
-height = int(cap.get(4))
-width = int(cap.get(3))
-name = videopath.split('.')[0]
-print('[INFO] VIDEO NAME : ' + name)
-print('[INFO] FPS : ' + str(fps))
-out = cv2.VideoWriter('new_'+name+'.avi', cv2.VideoWriter_fourcc(*'MJPG'), fps, (width,height))
-vocab = []
-prev_bounds = []
-frist = True
-prev_frame = 0
-n_m = 100
-print_text = True
-number_frame = 1
+		self.cap = cv2.VideoCapture(self.videopath)
+		self.vdo_name = self.videopath.split('.')[0]
+		# try:
+		self.cap.isOpened()
+		self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+		self.output_vdo_writer = self.create_vdo_output_writer()
+		
+		self.process_vdo()
+		# except Exception as e: print(e)
 
-print('[INFO] READY FOR DETECT AND RECOGNIZE \n')
-print('[INFO] THE LIST OF FOUND SENTENSES :')
+	def create_vdo_output_writer(self):
+		height = int(self.cap.get(4))
+		width = int(self.cap.get(3))
+		vdo_writer = cv2.VideoWriter('new_'+self.vdo_name+'.avi', cv2.VideoWriter_fourcc(*'MJPG'), self.fps, (width,height))
+		return vdo_writer
 
-while(cap.isOpened()):
-  print('number_frame ', number_frame)
-  bounds = []
-  ret, frame = cap.read() 
-  if ret == False:
-    print('[INFO] End Of Video...')
-    break
+	def process_vdo(self):
+		prev_bounds = []
+		is_first_frame = True
+		prev_frame = 0
+		n_m = 100
+		print_text = True
+		frame_idx = 1
 
-  #----- frame similarity ------
-  if(not frist):
-    n_m = compare_images(frame, prev_frame)
-  # if Manhattan norm(n_m) < 100 go to overlay process 
-  # if Manhattan norm(n_m) >= 100 go to detect process 
+		while(self.cap.isOpened()):
+			bounds = []
+			ret, frame = self.cap.read()
+			if ret == False:
+				print('[INFO] End Of Video...')
+				break
+			
+			if is_first_frame:
+				bounds = self.text_detection(frame)
+				if bounds == []:
+					result = bounds
+				else:
+					result = self.recognition(bounds, frame)
+					self.write_new_text_to_txt(result, frame_idx * (1.0 / self.fps))
+				is_first_frame = False
+			else:
+				if(self.is_frame_similar(prev_frame, frame)):
+					## Go for Overlay
+					self.overlay_text()
+				else:
+					## Go for Detect text
+					bounds = self.text_detection(frame)
+					if bounds == []:
+						result = bounds
+					else:
+						## Overlay_similarity_roi
+						bounds = self.overlay_similarity_roi(bounds, prev_bounds, frame, prev_frame)
+						result = self.recognition(bounds, frame)
+						self.write_new_text_to_txt(result, frame_idx * (1.0 / self.fps))
+						
+						
+			output_frame = self.draw_result(frame, result)
+			self.output_vdo_writer.write(output_frame)
 
-  if(n_m >= 100):
-    #------ detect --------
-    horizontal_list, free_list = reader.detect(frame)
-    for box in horizontal_list:
-      x_min = box[0]
-      x_max = box[1]
-      y_min = box[2]
-      y_max = box[3]
-      center = ( (x_min+x_max)/2, (y_min+y_max)/2 )
-      bounds.append([int(x_min), int(x_max), int(y_min), int(y_max), center, False, 'vtext',  'trantext', 0, 0, 'code_lang'])
+			### BOBO Sent progess to flask
+				
+			prev_frame = frame
+			prev_bounds = bounds
+			frame_idx += 1
+			print(frame_idx)
+		
+		out.release()
+		cap.release()
+		cv2.destroyAllWindows()
+		print('[INFO] Thank you')
+	
+	def recognition(self, bounds, frame):
+		if(self.is_multi_language()):
+			## Tesseract
+			## BOBO AIII
+			pass
+		else:
+			## easy_ocr
+			result = self.easy_ocr_recognition(bounds, frame)
+		return result
+	
+	def is_multi_language(self):
+		return False
 
-    if bounds == []:   
-      prev_bounds = bounds
-      pass
+	def is_frame_similar(self, prev_frame, frame):
+		# if Manhattan norm(n_m) < frame_similarity_threshold go to overlay process 
+		# if Manhattan norm(n_m) >= frame_similarity_threshold go to detect process 
+		n_m = compare_images(frame, prev_frame)
+		print(n_m)
+		if n_m >= self.frame_similarity_threshold:
+			return False
+		else:
+			print("frame is similar")
+			return True
+	
+	def is_roi_similar(self, roi, prev_roi):
+		return False
 
-    #----- text center similarity ------
-    # similarity = []
-    if(prev_bounds != []):
-      for index in range(len(bounds)):
-        c_b = bounds[index]
-        # similar = False
-        for i in range(len(prev_bounds)):
-          p_b = prev_bounds[i]
-          dist = math.dist(c_b[4], p_b[4])      # distance
-          if dist <= 6.0:                       # similar position by fine tune
-            #----- similarity ROI -----
-            # c_roi = frame[ c_b[2]:c_b[3], c_b[0]:c_b[1] ]
-            # p_roi = prev_frame[ c_b[2]:c_b[3], c_b[0]:c_b[1] ]
-            # n_m_roi = compare_images(c_roi, p_roi)
-            # if(n_m_roi < 100):
-              # similar = True
-              bounds[index][5] = i
-              bounds[index][6] = p_b[6] #vtext
-              bounds[index][7] = p_b[7] #tran_text
-              bounds[index][8] = p_b[8] #text_width
-              bounds[index][9] = p_b[9] #text_height
-              bounds[index][10] = p_b[10] #code_lang
-            # else:
-            #   bounds[index][5] = -1
-          # else:
-          #   bounds[index][5] = -1
-        # similarity.append(similar)       
-    # else:  # do the recognition process
-      # for i in range(len(bounds)):
-        # similarity.append(False)
-        # bounds[i][5] = -1
+	def text_detection(self, frame):
+		bounds = []
+    	#------ detect --------
+		horizontal_list, free_list = self.reader.detect(frame)
+		for box in horizontal_list:
+			x_min = box[0]
+			x_max = box[1]
+			y_min = box[2]
+			y_max = box[3]
+			center = ( (x_min+x_max)/2, (y_min+y_max)/2 )
+			bounds.append([int(x_min), int(x_max), int(y_min), int(y_max), center, -1, 'vtext',  'trantext', 0, 0, 'code_lang'])
+		return bounds
 
-    # 1 array
-    # bounds = 0x_min, 1x_max, 2y_min, 3y_max, 4center, 
-    # 5 # of similar prev frame or false, 6vtext, 7trantext(from prev or recognition), 
-    # 8text_width, 9text_height, 10code_lang
+	def overlay_similarity_roi(self, bounds, prev_bounds, frame, prev_frame):
+		for bound_idx in range(len(bounds)):
+			bound = bounds[bound_idx]
 
-    # # of similar prev frame if False mean did the recognize
+			for prev_bound_idx in range(len(prev_bounds)):
+				prev_bound = prev_bounds[prev_bound_idx]
+				dist = math.dist(bound[4], prev_bound[4])      # distance
+
+				if dist <= self.roi_distance_threshold:                       # similar position by fine tune
+					roi = frame[ bound[2]:bound[3], bound[0]:bound[1] ]
+					prev_roi = prev_frame[ prev_bound[2]:prev_bound[3], prev_bound[0]:prev_bound[1] ]
+
+					if(self.is_roi_similar(roi, prev_roi)):
+						## Overlay Similar_roi
+						bounds[index][5] = i
+						bounds[index][6] = prev_bound[6] #vtext
+						bounds[index][7] = prev_bound[7] #tran_text
+						bounds[index][8] = prev_bound[8] #text_width
+						bounds[index][9] = prev_bound[9] #text_height
+						bounds[index][10] = prev_bound[10] #code_lang
+		return bounds
 
     #------- recognition -----
-    # # need to display because we found the new word.
-    # if(lang == 'mult'): # use tesseract  and get src lang too for print
-    for index in range(len(bounds)):
-      c_b = bounds[index]
-      if(str(c_b[5]) == 'False'):   # similarity = False, so need to recognize and translate
-        print_text = True
-        c_roi = frame[ c_b[2]:c_b[3], c_b[0]:c_b[1] ]
-        c_rec = reader.recognize(c_roi)
-        text = c_rec[0][1] 
-        text = text.lower()
-        trans = translator.translate(text, lang_src=code_lang, lang_tgt=code_translang)
-        text_width, text_height = font.getsize(trans)
-        bounds[index][6] = text
-        bounds[index][7] = trans
-        bounds[index][8] = text_width
-        bounds[index][9] = text_height
-        bounds[index][10] = code_lang
-        
-    if(print_text):
-      print_text = False
-      #   totalsec = int(i//fps)
-      #   min = str(totalsec//60)  calculate again
-      #   sec = str(totalsec%60)
-      
-      # file = open("text.txt", "a")
-      # L = ["Current video time is ", hour, ':', min, ':', sec, "\n"]
-      # file.writelines(L)
-      for i in range(len(bounds)):
-        if(bounds[i][6] == ''):  
-            continue
-        # L = [ str(bounds[i][6]), " {", code_lang, "} : ", str(bounds[i][7]), '\n' ] 
-        # file.writelines(L) 
-        print(bounds[i])
-      # file.close()
-        
-    prev_frame = frame.copy()
-    prev_bounds = bounds  
-    frist = False
+	def easy_ocr_recognition(self, bounds, frame):
+		for index in range(len(bounds)):
+			c_b = bounds[index]
+			if(c_b[5] == -1):   # similarity = -1, so need to recognize and translate
+				print_text = True
+				c_roi = frame[ c_b[2]:c_b[3], c_b[0]:c_b[1] ]
+				c_rec = self.reader.recognize(c_roi)
+				text = c_rec[0][1] 
+				text = text.lower()
+				trans = translator.translate(text, lang_src=self.code_lang, lang_tgt=self.code_translang)
+				text_width, text_height = self.font.getsize(trans)
+				bounds[index][6] = text
+				bounds[index][7] = trans
+				bounds[index][8] = text_width
+				bounds[index][9] = text_height
+				bounds[index][10] = self.code_lang
+		return bounds
 
-  #------ overlay ------
-  for b in bounds:
-    startX, endX, startY, endY, position, number_prev_frame, ori_text,  tran_text, text_width, text_height, code_lang = b[:]
-    #---- transparent
-    if overlay == 'above':
-      X=startX; Y = startY - 5; Xwidth = startX+text_width; Yheight = startY-text_height; a = 0.7 
-      blk = np.zeros(frame.shape, np.uint8)
-    elif overlay == 'under':
-      X = startX; Y = endY+text_height; Xwidth = X+text_width; Yheight = Y-text_height; a = 0.7
-      blk = np.zeros(frame.shape, np.uint8)
-    else:
-      X = startX ;Y = startY; Xwidth = endX; Yheight = endY; a = 1
-      blk = np.zeros(frame.shape, np.uint8)
-    
-    cv2.rectangle(blk, (int(X), int(Y)), (int(Xwidth), int(Yheight)), (255, 255, 255), cv2.FILLED)
-    frame = cv2.addWeighted(frame, 1, blk, a, gamma=0);
+	def write_new_text_to_txt(self, result, vdo_time_sec):
+		conversion = datetime.timedelta(seconds=vdo_time_sec)
+		converted_time = str(conversion)
+		print(converted_time)
+		file = open("text.txt", "a")
+		L = ["Current video time is ", converted_time, "\n"]
+		file.writelines(L)
+		for i in range(len(result)):
+			if(result[i][6] == ''):
+				continue
+			L = (result[i][6] + " {" + str(self.code_lang) + "} : " + result[i][7] + '\n').encode("utf8") 
+			print(L)
+			## BOBO dont forget implement this dunction after tesseract added
+			file.writelines(str(L))
+			print(result[i])
+		file.close()
+		
+	
+	def draw_result(self, frame, result):
+		output_frame = frame
+		for r in result:
+			startX, endX, startY, endY, position, number_prev_frame, ori_text,  tran_text, text_width, text_height, code_lang = r[:]
+			#---- transparent
+			if self.overlay == 'above':
+				X=startX; Y = startY - 5; Xwidth = startX+text_width; Yheight = startY-text_height; a = 0.7
+			elif self.overlay == 'under':
+				X = startX; Y = endY+text_height; Xwidth = X+text_width; Yheight = Y-text_height; a = 0.7
+			else:
+				X = startX ;Y = startY; Xwidth = endX; Yheight = endY; a = 1
+		
+			blk = np.zeros(frame.shape, np.uint8)
+		
+			cv2.rectangle(blk, (int(X), int(Y)), (int(Xwidth), int(Yheight)), (255, 255, 255), cv2.FILLED)
+			output_frame = cv2.addWeighted(output_frame, 1, blk, a, gamma=0)
 
-    #---- rectangle
-    frame = cv2.rectangle(frame, (int(startX), int(startY)), (int(endX), int(endY)),code_color, 1)
-    
-    #---- text
-    img_pil = Image.fromarray(frame)
-    draw = ImageDraw.Draw(img_pil)
-    if overlay == 'above' or overlay == 'under':
-      Y = Y-text_height
-    else:
-      X = startX + (endX-startX)/2 - (text_width/2)
-      Y = Y+(text_height/2)
-    draw.text((X, Y), tran_text, font = font, fill = code_color)  # position, text, font, (b, g, r, a)
-    frame = np.array(img_pil)
-  
-  number_frame += 1
-  out.write(frame)
-    
-out.release()
-cap.release()
-cv2.destroyAllWindows()
-print('[INFO] Thank you')
+			#---- rectangle
+			output_frame = cv2.rectangle(output_frame, (int(startX), int(startY)), (int(endX), int(endY)),self.code_color, 1)
+		
+			#---- text
+			img_pil = Image.fromarray(output_frame)
+			draw = ImageDraw.Draw(img_pil)
+			if self.overlay == 'above' or self.overlay == 'under':
+				Y = Y-text_height
+			else:
+				X = startX + (endX-startX)/2 - (text_width/2)
+				Y = Y+(text_height/2)
+			draw.text((X, Y), tran_text, font = self.font, fill = self.code_color)  # position, text, font, (b, g, r, a)
+			output_frame = np.array(img_pil)
+		return output_frame
+
+def main():
+	rt_atrt = RT_ATRT()
+
+if __name__ == "__main__":
+    main()
